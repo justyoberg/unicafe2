a72ac6eb2da7c036652e56479190be8506d181bd
Justy <justyoberg1@gmail.com>
2024-09-15 15:06:31 -0700

    Add display for All, Average, and Positive percentage

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all = good + bad + neutral
+  const avg = (good - bad) / all || 0
+  const percent = (good / all) * 100 || 0
 
   return (
     <div>
@@ -19,6 +22,9 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {avg}</p>
+      <p>Positive: {percent} %</p>
     </div>
   )
 }
